564f5574920c236feababa4b4ad9d0bf4808280b
Stella <predevxyzlol@gmail.com>
2023-02-21 11:29:34 -0500

    Update botconfig.js

--- src/utils/botconfig.js
+++ src/utils/botconfig.js
@@ -2,4 +2,6 @@ module.exports = {
     memberRole: "1077432247847751821",
     welcomeChannel: "1077432249605169206",
     goodbyeChannel: "1077432249605169208",
+    qotdChannel: "1077432698563481712",
+    aotdChannel: "1077620659573751830",
 }
